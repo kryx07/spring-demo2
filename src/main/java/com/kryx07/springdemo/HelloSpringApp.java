605e573e4d4e5dfc71be9e932282d1dbde2063fa
kryx07 <kryx07@gmail.com>
2017-09-03 19:55:11 +0200

    Xml config file added.

---
+++ src/main/java/com/kryx07/springdemo/HelloSpringApp.java
@@ -0,0 +1,16 @@
+package com.kryx07.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Coach coach = applicationContext.getBean("myCoach", Coach.class);
+
+        System.out.println(coach.getDailyWorkout());
+    }
+}
--- src/main/java/com/kryx07/springdemo/MyApp.java
+++ src/main/java/com/kryx07/springdemo/MyApp.java
@@ -1,8 +1,11 @@
 package com.kryx07.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
 
     public static void main(String[] args) {
+
         Coach theCoach = new TrackCoach();
 
         System.out.println(theCoach.getDailyWorkout());
